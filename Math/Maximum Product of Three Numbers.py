2fc76232805b3faabc6fb34d47590011ab039011
Fathimath Shana AP <shanaap85@gmail.com>
2026-09-19 19:00:14 +0530

    Create Maximum Product of Three Numbers.py

---
+++ Math/Maximum Product of Three Numbers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        a = nums[-1]
+        if nums[0] < 0 and nums[1] < 0:
+            if nums[-1] > 0 :
+                if nums[0] * nums[1] *nums[-1] > nums[-1]* nums[-2]*nums[-3]:
+                    return nums[0] * nums[1] *nums[-1]
+        return nums[-1]* nums[-2]*nums[-3]
